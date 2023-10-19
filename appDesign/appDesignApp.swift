c638748d6cd7c1c8463bc1a574002c6816d9d04f
Ananya Tandel <anta1515@colorado.edu>
2023-10-19 10:43:44 -0600

    Initial Commit

---
+++ appDesign/appDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  appDesignApp.swift
+//  appDesign
+//
+//  Created by Ananya Tandel on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct appDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
